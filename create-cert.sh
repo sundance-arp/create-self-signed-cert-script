26e1158d7dfa15f25310fc949bc81057e6299f89
sundance-arp <black.meimei.0209@gmail.com>
2019-11-29 19:12:55 +0900

    Add create-cert.sh

---
+++ create-cert.sh
@@ -0,0 +1,39 @@
+#!/bin/bash -x
+
+# directories
+ca_cert_dir="./ca-cert"
+server_cert_dir="./server-cert"
+client_cert_dir="./client-cert"
+# common names
+ca_common_name="TEST CA"
+server_common_name="localhost"
+client_common_name="user"
+
+
+# create directories
+mkdir ${ca_cert_dir}
+mkdir ${server_cert_dir}
+mkdir ${client_cert_dir}
+
+
+# create ca certificate
+openssl genrsa 4096 > ${ca_cert_dir}/ca.key
+openssl req -new -key ${ca_cert_dir}/ca.key -subj "/CN=${ca_common_name}/C=JP" > ${ca_cert_dir}/ca.csr
+openssl x509 -req -in ${ca_cert_dir}/ca.csr -signkey ${ca_cert_dir}/ca.key -days 3650 -out ${ca_cert_dir}/ca.crt 
+
+# create server certificate
+openssl genrsa 4096 > ${server_cert_dir}/server.key
+openssl req -new -key ${server_cert_dir}/server.key -subj "/CN=${server_common_name}/C=JP" > ${server_cert_dir}/server.csr
+openssl x509 -req -in ${server_cert_dir}/server.csr -CA ${ca_cert_dir}/ca.crt -CAkey ${ca_cert_dir}/ca.key -CAcreateserial -days 3650 -out ${server_cert_dir}/server.crt
+
+# create client certificate
+openssl genrsa 4096 > ${client_cert_dir}/client.key
+openssl req -new -key ${client_cert_dir}/client.key -subj "/CN=${client_common_name}/C=JP" > ${client_cert_dir}/client.csr
+openssl x509 -req -in ${client_cert_dir}/client.csr -CA ${ca_cert_dir}/ca.crt -CAkey ${ca_cert_dir}/ca.key -CAcreateserial -days 3650 -out ${client_cert_dir}/client.crt
+
+
+# created files
+echo '####################'
+ls ${ca_cert_dir}/
+ls ${server_cert_dir}/
+ls ${client_cert_dir}/
